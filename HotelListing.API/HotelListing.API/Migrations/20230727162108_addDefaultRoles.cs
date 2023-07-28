28a6e529f94b5a198508b4f2269539ae631a6683
Ezequiel Lorenz <eze_lorenz@hotmail.com>
2023-07-28 12:29:48 -0300

    Added Identityand Token Authentication

---
+++ HotelListing.API/HotelListing.API/Migrations/20230727162108_addDefaultRoles.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace HotelListing.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class addDefaultRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "64225f2d-961c-4be2-b01e-5e42b5406412", null, "User", "USER" },
+                    { "9565493d-7cca-493c-8661-5ef64aaa131d", null, "Administrator", "ADMINISTRATOR" }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { "Negril", "Sandals Resort and Spa" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { "George Town", "Comfort Suites" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Address",
+                value: "Nassua");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "64225f2d-961c-4be2-b01e-5e42b5406412");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9565493d-7cca-493c-8661-5ef64aaa131d");
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { " Negril", "Sandals Resort & Spa" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Address", "Name" },
+                values: new object[] { " George Town", "Confort Suites" });
+
+            migrationBuilder.UpdateData(
+                table: "Hotels",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Address",
+                value: " Nassua");
+        }
+    }
+}
